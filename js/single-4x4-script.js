bdb32f1e7d81b46093176e8a00abdc00a72da025
Pathogene2 <anthony.mensah@amalitech.org>
2022-11-29 23:49:12 +0000

    button functionality

--- js/single-4x4-script.js
+++ js/single-4x4-script.js
@@ -1,30 +1,31 @@
-let minutes= document.getElementById('minutes');
-let seconds= document.getElementById('seconds');
-let min = 0;
-let sec = 0;
-let interval;
+let num = document.querySelector('#num');
+let icon = document.querySelector('#icon');
+let num1 = document.querySelector('#num1');
+let num2 = document.querySelector('#num2');
+let num3 = document.querySelector('#num3');
+let num4 = document.querySelector('#num4');
+let four = document.querySelector('#four');
+let six = document.querySelector('#six');
+let strtBn = document.querySelector('#strtBn')
+num.style.backgroundColor='#304859';
+num1.style.backgroundColor='#304859';
+four.style.backgroundColor='#304859'
 
-let logic=()=>{
-	sec++;
-	if(sec > 59 && sec%60 === 0){
-		min+=1;
-		if(min<=9) {
-			minutes.innerHTML=`0${min}`
-			} 
+
+function backgroundChange(variable){
+	let content = variable.parentElement.children;
+	for(let i=0; i<content.length; i++){
+		if(content[i].id==variable.id){
+			document.querySelector(`#${content[i].id}`).style.backgroundColor='#304859'
+		}
 		else{
-			minutes.innerHTML=`${min}`;
-		}
-		sec= 0;
-	};
-	if(sec<=9) {
-		seconds.innerHTML=`0${sec}`
-		}
-	else{
-		seconds.innerHTML=`${sec}`
+			document.querySelector(`#${content[i].id}`).style.backgroundColor=''
+		}		
 	}
 }
 
-let timeRunner= function(){
-	interval = setInterval(logic(),1000)
-};
-timeRunner()
+function era (backgroundChange){
+	if(document.querySelector(`#${backgroundChange.content[i].id}`).style.backgroundColor='#304859'){
+		console.log(content[i].id)
+	}
+}
